abb601d0bd826110b85e2aa0338e1af7ff8a12cd
Qatora <qatorabataka@gmail.com>
2023-06-09 20:52:06 +0330

    Tutorial -> What is CPP

--- main.cpp
+++ main.cpp
@@ -7,3 +7,20 @@
      */
     # define __CPP_TIME
 # endif // __CPP_TIME
+
+# ifndef __WHAT_IS_CPP
+    /**
+     * @brief C++ is a High-Level Programming Language
+     * Used for Developing a Wide Variety of Applications
+     * It was Developed as an Extension of The C Language
+     * @details C++ is Known for Its Performance,
+     * Flexibility, and Portability, Making It a Popular
+     * Choice for Complex and Resource-Intensive Projects
+     * @note Some of C++ Usage :
+     * - Operating Systems
+     * - Device Drivers
+     * - Video Games
+     * - Embedded Software
+     */
+    # define __WHAT_IS_CPP
+# endif __WHAT_IS_CPP
