d3d73d7717420ce586e5f5c9a8c0bea77a9ac20f
Qatora <qatorabataka@gmail.com>
2023-06-09 20:50:41 +0330

    CPP Time

---
+++ main.cpp
@@ -0,0 +1,9 @@
+# ifndef __CPP_TIME
+    /**
+     * @brief CPPTime's Macro Definition
+     * @attention Follow Our Daily Posts
+     * @date 2023-05-16
+     * @version 1.0.0
+     */
+    # define __CPP_TIME
+# endif // __CPP_TIME
